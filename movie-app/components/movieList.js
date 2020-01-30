e9e93af24b3f185e2ab49f112be0abaa079fc3b6
fochykun <dvchervakov@gmail.com>
2020-01-30 13:35:23 +0300

    counters
    add increment and decrement buttons to change count

--- movie-app/components/movieList.js
+++ movie-app/components/movieList.js
@@ -1,106 +1,128 @@
 import React from 'react'
 
-const MovieList = () => {
+class MovieList extends React.Component{
 
-  return (
-    <React.Fragment>
-      <div className="col-lg-4 col-md-6 mb-4">
-        <div className="card h-100">
-          <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-          <div className="card-body">
-            <h4 className="card-title">
-              <a href="#">Item One</a>
-            </h4>
-            <h5>$24.99</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+  constructor(props){
+    super(props)
+    this.state={
+      count:0
+    }
+  }
+
+  increment = () =>{
+    this.setState({count:this.state.count+1});
+  }
+
+  decrement = () =>{
+    this.setState({count:this.state.count-1});
+  }
+  
+  render(){
+    return (
+      <React.Fragment>
+        <div>
+          <button onClick={this.increment} className="btn btn-primary">Inctement number</button>
+          <button onClick={this.decrement} className="btn btn-primary">Dectement number</button>
+          <div>count {this.state.count}</div>
+        </div>
+        <div className="col-lg-4 col-md-6 mb-4">
+          <div className="card h-100">
+            <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+            <div className="card-body">
+              <h4 className="card-title">
+                <a href="#">Item One</a>
+              </h4>
+              <h5>$24.99</h5>
+              <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+            </div>
+            <div className="card-footer">
+              <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div className="col-lg-4 col-md-6 mb-4">
-        <div className="card h-100">
-          <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-          <div className="card-body">
-            <h4 className="card-title">
-              <a href="#">Item Two</a>
-            </h4>
-            <h5>$24.99</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
-        </div>
-      </div>
-
-      <div className="col-lg-4 col-md-6 mb-4">
-        <div className="card h-100">
-          <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-          <div className="card-body">
-            <h4 className="card-title">
-              <a href="#">Item Three</a>
-            </h4>
-            <h5>$24.99</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+  
+        <div className="col-lg-4 col-md-6 mb-4">
+          <div className="card h-100">
+            <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+            <div className="card-body">
+              <h4 className="card-title">
+                <a href="#">Item Two</a>
+              </h4>
+              <h5>$24.99</h5>
+              <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="card-footer">
+              <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div className="col-lg-4 col-md-6 mb-4">
-        <div className="card h-100">
-          <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-          <div className="card-body">
-            <h4 className="card-title">
-              <a href="#">Item Four</a>
-            </h4>
-            <h5>$24.99</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
-        </div>
-      </div>
-
-      <div className="col-lg-4 col-md-6 mb-4">
-        <div className="card h-100">
-          <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-          <div className="card-body">
-            <h4 className="card-title">
-              <a href="#">Item Five</a>
-            </h4>
-            <h5>$24.99</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+  
+        <div className="col-lg-4 col-md-6 mb-4">
+          <div className="card h-100">
+            <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+            <div className="card-body">
+              <h4 className="card-title">
+                <a href="#">Item Three</a>
+              </h4>
+              <h5>$24.99</h5>
+              <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+            </div>
+            <div className="card-footer">
+              <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div className="col-lg-4 col-md-6 mb-4">
-        <div className="card h-100">
-          <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-          <div className="card-body">
-            <h4 className="card-title">
-              <a href="#">Item Six</a>
-            </h4>
-            <h5>$24.99</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+  
+        <div className="col-lg-4 col-md-6 mb-4">
+          <div className="card h-100">
+            <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+            <div className="card-body">
+              <h4 className="card-title">
+                <a href="#">Item Four</a>
+              </h4>
+              <h5>$24.99</h5>
+              <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+            </div>
+            <div className="card-footer">
+              <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+            </div>
           </div>
         </div>
-      </div>
-    </React.Fragment>
-  )
+  
+        <div className="col-lg-4 col-md-6 mb-4">
+          <div className="card h-100">
+            <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+            <div className="card-body">
+              <h4 className="card-title">
+                <a href="#">Item Five</a>
+              </h4>
+              <h5>$24.99</h5>
+              <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="card-footer">
+              <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+            </div>
+          </div>
+        </div>
+  
+        <div className="col-lg-4 col-md-6 mb-4">
+          <div className="card h-100">
+            <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
+            <div className="card-body">
+              <h4 className="card-title">
+                <a href="#">Item Six</a>
+              </h4>
+              <h5>$24.99</h5>
+              <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+            </div>
+            <div className="card-footer">
+              <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default MovieList
--- movie-app/components/sideMenu.js
+++ movie-app/components/sideMenu.js
@@ -1,17 +1,34 @@
-
+import { useState } from 'react'
 
 
 const SideMenu = () => {
-    return (
-     <div>
-       <h1 className="my-4">Shop Name</h1>
-        <div className="list-group">
-          <a href="#" className="list-group-item">Category 1</a>
-          <a href="#" className="list-group-item">Category 2</a>
-          <a href="#" className="list-group-item">Category 3</a>
-        </div>
-     </div>
-    )
+  const [count, setCount] = useState(0);
+
+  const increment = () =>{
+    setCount(count+1);
   }
+
+  const decrement = () =>{
+    setCount(count-1);
+  }
+
+  return (
+    <div>
+      <h1 className="my-4">Shop Name</h1>
+      <div className="list-group">
+        <a href="#" className="list-group-item">Category 1</a>
+        <a href="#" className="list-group-item">Category 2</a>
+        <a href="#" className="list-group-item">Category 3</a>
+      </div>
+
+      <div>
+        <button onClick={increment} className="btn btn-primary">Inctement number</button>
+        <button onClick={decrement} className="btn btn-primary">Dectement number</button>
+        <div>count {count}</div>
+      </div>
+
+    </div>
+  )
+}
   
   export default SideMenu
